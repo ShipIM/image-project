af863b2f759de760722295444352a3ee4c17225b
ShipIM <ilya.shipunov@gmail.com>
2024-04-09 20:42:58 +0300

    add image_filter entity

---
+++ src/main/java/com/example/imageproject/model/entity/ImageFilter.java
@@ -0,0 +1,33 @@
+package com.example.imageproject.model.entity;
+
+import com.example.imageproject.model.enumeration.ImageStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name = "image_filter")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ImageFilter {
+
+    @Id
+    private Long id;
+
+    private ImageStatus status;
+
+    @Column("original_image_id")
+    private String original;
+
+    @Column("modified_image_id")
+    private String modified;
+
+    @Column("request_id")
+    private String request;
+
+}
